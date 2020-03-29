3fc1b96f95ea0483539e78e1647e2dbfd9a19dbd
Porson <porsonlee@gmail.com>
2020-03-28 23:30:17 -0700

    implemented so it clears the text input after you click submit

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,15 @@
 const chatForm = document.getElementById('chat-form');
+const chatMessages = document.querySelector('.chat-messages');
 
 const socket = io();
 
 // Message from server
 socket.on('message', message => {
+  console.log(message);
   outputMessage(message);
+
+  // Scroll down
+  chatMessages.scrollTop = chatMessages.scrollHeight;
 })
 
 // Message submit
@@ -16,6 +21,10 @@ chatForm.addEventListener('submit', (e) => {
 
   // Emit message to server
   socket.emit('chatMessage', msg);
+
+  // Clear inputs 
+  e.target.elements.msg.value = '';
+  e.target.elements.msg.focus();
 })
 
 // Output message to DOM
