8ad7c2c2fc77ea2227917e3b6f1be506c6146c26
Porson <porsonlee@gmail.com>
2020-03-28 23:22:57 -0700

    output messages now shows up in chatscreen on front end

--- server.js
+++ server.js
@@ -25,6 +25,11 @@ io.on('connection', socket => {
     io.emit('message', 'A user has left the chat');
   });
 
+  // Listen for chatMessage
+  socket.on('chatMessage', (msg) => {
+    io.emit('message', msg);
+  });
+
 });
 
 const PORT = 3000 || process.env.PORT;
