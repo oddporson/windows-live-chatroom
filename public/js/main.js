31274bea15b84812f28e63a51443297f58dd7293
Porson <porsonlee@gmail.com>
2020-03-29 20:57:13 -0700

    added room name to dom and users to dom

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,7 @@
 const chatForm = document.getElementById('chat-form');
 const chatMessages = document.querySelector('.chat-messages');
+const roomName = document.getElementById('room-name');
+const userList = document.getElementById('users');
 
 // Get username and room from URL
 const { username, room } = Qs.parse(location.search, {
@@ -54,5 +56,12 @@ function outputMessage(message){
 
 // Add room name to DOM
 function outputRoomName(room) {
-  
+  roomName.innerText = room;
+}
+
+// Add users to DOM 
+function outputUsers(users) {
+  userList.innerHTML = `
+  ${users.map(user => `<li>${user.username}</li>`).join('')}
+  `;
 }
